66af64f37e50ab8b6d8d816eceae3790d6c55c71
Omkar Kolate <53403067+omkarkolate@users.noreply.github.com>
2021-09-15 12:06:11 +0530

    feat: Added data context and reducer

---
+++ src/dataProvider/DataProvider.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useReducer } from "react";
+import { dataReducer } from "./DataReducer";
+
+const DataContext = createContext();
+
+export function useData() {
+	return useContext(DataContext);
+}
+
+export function DataProvider({ children }) {
+	const [state, dispatch] = useReducer(dataReducer, {
+		videos: [],
+		userId: "",
+		firstName: "",
+		lastName: "",
+		emailId: "",
+		password: "",
+		likedVideos: [],
+		watchLater: [],
+		playlist: [],
+		history: []
+	});
+
+	const apiURL = process.env.REACT_APP_apiURL;
+
+	return (
+		<DataContext.Provider value={{ state, dispatch, apiURL }}>
+			{children}
+		</DataContext.Provider>
+	);
+}
---
+++ src/dataProvider/DataReducer.js
@@ -0,0 +1,73 @@
+export function dataReducer(state, { type, payload }) {
+	switch (type) {
+		case "ADD_PRODUCTS":
+			return { ...state, products: payload };
+
+		case "SAVE_USER":
+			return { ...state, userId: payload._id, ...payload };
+
+		case "UPDATE_USER":
+			return { ...state, ...payload };
+
+		case "ADD_TO_WATCH_LATER":
+			return {
+				...state,
+				watchLater: [...state.watchLater, payload]
+			};
+
+		case "REMOVE_FROM_WATCH_LATER":
+			return {
+				...state,
+				watchLater: state.watchLater.filter(
+					({ _id }) => _id !== payload
+				)
+			};
+
+		case "ADD_TO_LIKED_VIDEOS":
+			return {
+				...state,
+				likedVideos: [...state.likedVideos, payload]
+			};
+
+		case "REMOVE_FROM_LIKED_VIDEOS":
+			return {
+				...state,
+				likedVideos: state.likedVideos.filter(
+					({ _id }) => _id !== payload
+				)
+			};
+
+		case "ADD_TO_PLAYLIST":
+			return {
+				...state,
+				playlist: [...state.playlist, payload]
+			};
+
+		case "REMOVE_FROM_PLAYLIST":
+			return {
+				...state,
+				playlist: state.playlist.filter(({ _id }) => _id !== payload)
+			};
+
+		case "ADD_TO_HISTORY":
+			return {
+				...state,
+				history: [...payload, ...state.history]
+			};
+
+		case "REMOVE_FROM_HISTORY":
+			return {
+				...state,
+				history: state.history.filter(({ _id }) => _id !== payload)
+			};
+
+		case "CLEAR_HISTORY":
+			return { ...state, history: payload };
+
+		case "RESET_USER_DATA":
+			return { ...state, ...payload };
+
+		default:
+			return state;
+	}
+}
